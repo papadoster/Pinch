ee10c22c8fd3aa5f630203b02de9f3af9beb245d
Aleksandr Karpov <papadoster13@gmail.com>
2023-02-16 22:07:57 +0100

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Александр Карпов on 16.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
